34dfeb1df6e571e7b2168980ade2b2525a755e08
adjo <calvin@kairuconsulting.com>
2020-06-10 20:22:21 -0400

    sample button added

--- blackmencodeapp/BlkMenCodeLandingPage.xaml.cs
+++ blackmencodeapp/BlkMenCodeLandingPage.xaml.cs
@@ -1,15 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-
+﻿using blackmencodeapp.viewModel;
 using Xamarin.Forms;
 
 namespace blackmencodeapp
 {
     public partial class BlkMenCodeLandingPage : ContentPage
     {
+        //LandingPageViewModel viewModel;
+
         public BlkMenCodeLandingPage()
         {
             InitializeComponent();
+            BindingContext = new LandingPageViewModel();
+            //BindingContext = viewModel = new LandingPageViewModel();
+        }
+
+        void testSwitch_Toggled(System.Object sender, Xamarin.Forms.ToggledEventArgs e)
+        {
+            Application.Current.MainPage.DisplayAlert("Black Men Code", "BlackMenCode", "Toggle changed to" + ((Switch)(sender)).IsToggled);
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            Application.Current.MainPage.DisplayAlert("Black Men Code", "BlackMenCode", "Button Clicked");
         }
     }
 }
---
+++ blackmencodeapp/viewModel/LandingPageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace blackmencodeapp.viewModel
+{
+    public class LandingPageViewModel
+    {
+        public string persons_name { get; set; } = "Black Men Code Names Here";
+
+        public LandingPageViewModel()
+        {
+           
+        }
+    }
+}
